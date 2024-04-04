1345479bac15cc99387e738f686417eb43e86be8
Dominik Jalč <dominik@devbros.cz>
2024-04-04 23:07:33 +0200

    feat: adjust jj and kk keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jj", "<Esc>", { desc = "Esc" })
-vim.keymap.set("i", "kk", "<Esc>", { desc = "Esc" })
+vim.keymap.set("i", "jj", "<Esc>:normal j<CR>", { desc = "Esc and down" })
+vim.keymap.set("i", "kk", "<Esc>:normal k<CR>", { desc = "Esc and up" })
