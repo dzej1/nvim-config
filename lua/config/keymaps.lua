1b1a2241b6d382223d79136df96e7377821b6153
Dominik Jalč <dominik@devbros.cz>
2024-04-08 11:50:54 +0200

    feat: add copy file name with relative path keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,4 @@
 
 vim.keymap.set("i", "jj", "<Esc>:normal j<CR>", { desc = "Esc and down" })
 vim.keymap.set("i", "kk", "<Esc>:normal k<CR>", { desc = "Esc and up" })
+vim.keymap.set("n", "cp", ':let @+ = expand("%")<CR>', { desc = "Copy filename" })
