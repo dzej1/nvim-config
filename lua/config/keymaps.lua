0481bed6c038ac12a29122237f4f1388fc266d76
Dominik Jalč <dominik@devbros.cz>
2024-05-05 14:37:52 +0200

    feat: add blame.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,5 @@ vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set({ "n", "x" }, "<C-0>", ":BlameToggle virtual<CR>")
